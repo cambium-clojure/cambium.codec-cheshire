a156a9a9dfb4c97c4ffdfd572f124bd01800aadc
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-03-20 17:35:55 +0530

    upgrade Cheshire version to 5.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 ## [WIP] 0.9.2 / 2018-March-??
 
-- [TODO] Upgrade Cheshire version to `5.8.0` (Jackson `2.9.0`)
+- Upgrade Cheshire version to `5.8.0` (Jackson `2.9.0`)
 
 
 ## 0.9.1 / 2017-September-11
--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.2.0"
   :pedantic? :abort
-  :dependencies [[cheshire "5.7.0"]]
+  :dependencies [[cheshire "5.8.0"]]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.5.1"]]}
@@ -13,5 +13,5 @@
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :c19 {:dependencies [[org.clojure/clojure "1.9.0-alpha20"]]}
+             :c19 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
