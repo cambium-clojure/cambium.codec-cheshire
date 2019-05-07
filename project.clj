cb28f55e87ebe6bff9fe86ae450967bcda9d1dca
Shantanu Kumar <kumar.shantanu@gmail.com>
2019-05-07 12:58:52 +0530

    add Clojure 1.10 test profile

--- project.clj
+++ project.clj
@@ -10,9 +10,10 @@
                 *warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :c15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :c19 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :c05 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :c06 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :c07 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :c08 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :c09 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :c10 {:dependencies [[org.clojure/clojure "1.10.1-beta2"]]}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
